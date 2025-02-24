24e5a15314990f89f072448ec9654a478f904705
Aaron Schulze <aaronschulze01@gmail.com>
2025-02-24 18:03:00 +0100

    added argument parsing

---
+++ equivalent-training-ReLUnetwork-SNN/pytorch/main_torch.py
@@ -0,0 +1,41 @@
+import argparse
+
+override = None       # hard-code args parameters instead of passing them over the CLI
+
+'''
+    Command-line argument parsing
+'''
+# Architecture setup
+strtobool = (lambda s: s=='True')       # For parsing boolean CLI args parameters
+parser = argparse.ArgumentParser(description='TTFS')
+parser.add_argument('--data_name', type=str, default='MNIST', help='(MNIST|CIFAR10|CIFAR100)')                              # name of the dataset to use
+parser.add_argument('--logging_dir', type=str, default='./logs/', help='Directory for logging')         
+parser.add_argument('--model_type', type=str, default='SNN', help='(SNN|ReLU)')                                             # choose between SNN and ReLU                              
+parser.add_argument('--model_name', type=str, default='FC2', help='Should contain (FC2|VGG[BN]): e.g. VGG_BN_test1')
+
+# Hyperparameters
+parser.add_argument('--lr', type=float, default=0.0005, help='Learning rate')
+parser.add_argument('--batch_size', type=int, default=8, help='Batch size')
+parser.add_argument('--epochs', type=int, default=10, help='Epochs. 0 -skip training')
+
+# Train, test and load modes
+parser.add_argument('--testing', type=strtobool, default=True, help='Execute testing.')
+parser.add_argument('--load', type=str, default='False', help='Load before training. (True|False|custom_name.h5)')          # Load or save the trained network weights
+parser.add_argument('--save', type=strtobool, default=False, help='Store after training.')
+
+# Robustness parameters:
+parser.add_argument('--noise', type=float, default=0.0, help='Noise std.dev.')
+parser.add_argument('--time_bits', type=int, default=0, help='number of bits to represent time. 0 -disabled')
+parser.add_argument('--weight_bits', type=int, default=0, help='number of bits to represent weights. 0 -disabled')
+parser.add_argument('--w_min', type=float, default=-1.0, help='w_min to use if weight_bits is enabled')
+parser.add_argument('--w_max', type=float, default=1.0, help='w_max to use if weight_bits is enabled')
+parser.add_argument('--latency_quantiles', type=float, default=0.0, help='Number of quantiles to take into account when calculating t_max. 0 -disabled')
+parser.add_argument('--mode', type=str, default='', help='Ignore: A hack to address a bug in argsparse during debugging')
+
+# Returns a tuple: args[0] is a Namespace with all the known parameters from 'parser' and 'override', args[1] contains ignored unknown parameters 
+args = parser.parse_known_args(override)
+if(len(args[1])>0):
+    print("Warning: Ignored args", args[1])
+# print(args)
+args = args[0]
+
