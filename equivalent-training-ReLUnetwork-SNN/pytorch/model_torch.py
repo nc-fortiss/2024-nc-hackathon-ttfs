111d31ed3cb140ad097ad47d1124d90487eb2b4f
Aaron Schulze <aaronschulze01@gmail.com>
2025-02-27 15:39:52 +0100

    defined SNN FC architecture and SpikingDense Layer (beginning)

--- equivalent-training-ReLUnetwork-SNN/pytorch/model_torch.py
+++ equivalent-training-ReLUnetwork-SNN/pytorch/model_torch.py
@@ -10,12 +10,48 @@ import torch.nn.functional as F
 
 
 class SpikingDenseTorch(nn.Module):
-    ''' Creates a single Spiking Dense Layer '''
-
-    def __init__(self):
-        super().__init__()
+    ''' Creates a single Spiking Dense Layer 
     
-    # TODO: define Spiking Layers in torch
+        Attributes:
+            N_in, N_out: number of input/output neurons of this layer
+            X_n: together with 'B_n', regulates spiking window size for this layer. 
+                In case of the output layer, then 'B_n' is kept small to avoid spike-generation as these spikes are not relevant anymore
+            t_min_prev: 't_min' from the spiking-time-window of the previous layer
+            t_min, t_max: 't_min' and 't_max' timestamps of the current layer
+            robustness_params = # TODO
+            alpha: # TODO
+            regularizer = # TODO
+            initializer = # TODO
+    '''
+
+    def __init__(self, N_in, N_out, X_n=1, robustness_params={}, kernel_regularizer=None, kernel_initializer=None):
+        super(SpikingDenseTorch).__init__()
+
+        self.N_in = N_in
+        self.N_out = N_out
+        self.X_n = X_n
+        self.B_n = (1 + 0.5) * X_n
+        self.t_min_prev, self.t_min, self.t_max=0, 0, 1
+        self.robustness_params=robustness_params
+        self.alpha = torch.ones(self.N_in, dtype=torch.float64)
+        self.regularizer = kernel_regularizer
+        self.initializer = kernel_initializer
+
+    def set_intervals(self, t_min_prev,t_min):
+        ''' Sets t_min_prev, t_min, and t_max for this layer. The bounds are determined and set 
+            before training even begins. Equivalent to 'set_params' in the tensorflow code. 
+        
+        '''
+        self.t_min_prev = t_min_prev 
+        self.t_min = t_min 
+        self.t_max = t_min + self.B_n 
+        return t_min, t_min+self.B_n
+
+    
+
+
+
+
 
 
 class FC_ReLU_torch(nn.Module):
@@ -28,7 +64,7 @@ class FC_ReLU_torch(nn.Module):
         N: returns N_hid[l] if N_hid is a list, else returns the constant N_hid value
         hidden_layers: list of torch.nn modules ()
         # TODO: input_layer, output_layer:
-    '''
+    ''' 
     def __init__(self, layers, N_hid,N_in, N_out):
         super().__init__()
         self.N_layers=layers 
@@ -43,6 +79,7 @@ class FC_ReLU_torch(nn.Module):
         self.input_layer = nn.Linear(self.N_in, self.N(1), dtype=torch.float64)
         self.hidden_layers = nn.ModuleList()
 
+        # !!!!!!                                                                      TODO !!! -> ADJUST LOOP TO ACCOUNT FOR CORRECT N. OF HIDDEN LAYERS 
         for i in range(self.N_layers-2):
             self.hidden_layers.append(nn.Linear(self.N(i+2), self.N(i+2), dtype=torch.float64)) 
 
@@ -86,7 +123,6 @@ class FC_ReLU_torch(nn.Module):
 
                 _, preds  = torch.max(outputs, dim=1)
 
-
                 train_acc += torch.sum(preds == target)
                 total += len(preds)
 
@@ -98,8 +134,35 @@ class FC_ReLU_torch(nn.Module):
 
         print('Finished Training')
 
+class FC_SNN_torch(nn.Module):
+    ''' Defines instance of a fully-connected SNN model
+    
+    
+    '''
+    def __init__(self, layers, N_hid, N_in, N_out, X_n, robustness_params):
+        super(FC_SNN_torch).__init__()
+        self.N_layers=layers 
+        self.N_hid = N_hid
+        self.N_in = N_in 
+        self.N_out = N_out 
+        self.X_n = X_n
 
-def torch_fc_model_ReLU(layers=2, N_hid=340,N_in=784, N_out=10):
-    ''' Returns instance of a fully-connected ReLU network '''
+        self.N = lambda l: (N_hid[l-1] if type(N_hid)==list else N_hid)
+
+        self.input_layer = SpikingDenseTorch(self.N_in, self.N(1), (X_n[0] if type(X_n)==list else X_n), robustness_params)
+        self.hidden_layers = nn.ModuleList()
+
+        for i in range(self.N_layers-2):
+            self.hidden_layers.append(SpikingDenseTorch(self.N(i+2), self.N(i+2), (X_n[i+1] if type(X_n)==list else X_n))) 
+
+        self.output_layer = SpikingDenseTorch(self.N(self.N_layers), self.N_out, robustness_params)
+
+        
+
+def create_torch_fc_model_ReLU(layers=2, N_hid=340,N_in=784, N_out=10):
+    ''' Returns instance of a fully-connected ReLU model '''
     return FC_ReLU_torch(layers, N_hid, N_in, N_out)
 
+def create_torch_fc_model_SNN(layers=2, N_hid=340, N_in=784, N_out=10, X_n=1000, robustness_params={}):
+    ''' Returns an instance of a fully-connected SNN model '''
+    return FC_SNN_torch(layers,N_hid,N_in,N_out,X_n,robustness_params, kernel_regularizer=None, kernel_initializer=None)
