62a9eebe2f7fd2bd5a50ddb1f68d2cb34225a2f5
Aaron Schulze <aaronschulze01@gmail.com>
2025-03-04 09:41:32 +0100

    defined spiking forward pass in FC_SNN

--- equivalent-training-ReLUnetwork-SNN/pytorch/model_torch.py
+++ equivalent-training-ReLUnetwork-SNN/pytorch/model_torch.py
@@ -1,6 +1,8 @@
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.nn.init as init
+
 
 # import pdb   # debugger
 
@@ -8,6 +10,18 @@ import torch.nn.functional as F
     Module implementing the pytorch version of the neural network architectures. 
 '''
 
+def call_spiking(tj, W, D_i, t_min_prev, t_min, t_max, robustness_params):
+    # Calculate the spiking threshold (Eq. 18)
+    threshold = t_max - t_min - D_i
+    # Calculate output spiking time ti (Eq. 7)
+    ti = (torch.matmul(tj-t_min, W) + threshold + t_min)
+
+    # Ensure valid spiking time. Do not spike for ti >= t_max.
+    # No spike is modelled as t_max that cancels out in the next layer (tj-t_min) as t_min there is t_max
+    ti = torch.where(ti < t_max, ti, t_max)
+    # Add noise to the spiking time for noise simulations
+    ti = ti + torch.normal(std=robustness_params['noise'], size=ti.shape, dtype=torch.float64)
+    return ti
 
 class SpikingDenseTorch(nn.Module):
     ''' Creates a single Spiking Dense Layer 
@@ -16,27 +30,39 @@ class SpikingDenseTorch(nn.Module):
             N_in, N_out: number of input/output neurons of this layer
             X_n: together with 'B_n', regulates spiking window size for this layer. 
                 In case of the output layer, then 'B_n' is kept small to avoid spike-generation as these spikes are not relevant anymore
-            t_min_prev: 't_min' from the spiking-time-window of the previous layer
-            t_min, t_max: 't_min' and 't_max' timestamps of the current layer
+            t_min_prev: corresponds to the 't_min' from the spiking-time-window of the previous layer
+            t_min, t_max: represent the minimum and maximum spike times for the spiking-time-window of the current layer
             robustness_params = # TODO
             alpha: # TODO
             regularizer = # TODO
             initializer = # TODO
+            kernel: # TODO // trainable neuron weight
+            is_output: boolean flag, True if self is the output layer
     '''
 
-    def __init__(self, N_in, N_out, X_n=1, robustness_params={}, kernel_regularizer=None, kernel_initializer=None):
+    def __init__(self, N_in, N_out, X_n=1, robustness_params={}, kernel_regularizer=None, kernel_initializer=None, is_output=False):
         super().__init__()
 
-        self.N_in = N_in
+        self.N_in = N_in            
         self.N_out = N_out
         self.X_n = X_n
         self.B_n = (1 + 0.5) * X_n
         self.t_min_prev, self.t_min, self.t_max=0, 0, 1
+        self.is_output = is_output
+
         self.robustness_params=robustness_params
         self.alpha = torch.ones(self.N_in, dtype=torch.float64)
         self.regularizer = kernel_regularizer
         self.initializer = kernel_initializer
 
+        self.kernel = nn.Parameter(torch.empty((N_in,N_out), dtype=torch.float64))    # register weight in model
+        if kernel_initializer: 
+            i = 0
+        else: 
+            init.xavier_uniform_(self.kernel)           # initialize weight tensor, with initializer if provided
+
+        self.D_i = nn.Parameter(torch.zeros(N_in, N_out))
+        
     def set_intervals(self, t_min_prev,t_min):
         ''' Sets t_min_prev, t_min, and t_max for this layer. The bounds are determined and set 
             before training even begins. Equivalent to 'set_params' in the tensorflow code. 
@@ -48,9 +74,17 @@ class SpikingDenseTorch(nn.Module):
         return t_min, t_min+self.B_n
 
     
-
-
-
+    def forward(self, tj):
+        ''' Defines a single pass through the Spiking Dense Layer.
+            The input 'tj' represents the spike times that were integrated 
+            from the previous layer, thus transforming them into output spike times 'ti'
+        '''
+        ti = call_spiking(tj, self.kernel, self.D_i, self.t_min_prev, self.t_min, self.t_max, self.robustness_params)
+        if (self.is_output):
+            W_mult_x = torch.matmul(self.t_min-tj, self.kernel)
+            self.alpha = self.D_i/(self.t_min-self.t_min_prev)
+            ti = self.alpha * (self.t_min - self.t_min_prev) + W_mult_x
+        return ti
 
 
 
@@ -148,7 +182,7 @@ class FC_SNN_torch(nn.Module):
     
     
     '''
-    def __init__(self, layers, N_hid, N_in, N_out, X_n, robustness_params, kernel_regularizer, kernel_initializer):#
+    def __init__(self, layers, N_hid, N_in, N_out, X_n, robustness_params, kernel_regularizer, kernel_initializer):
         super().__init__()
         self.N_layers=layers 
         self.N_hid = N_hid
@@ -162,12 +196,19 @@ class FC_SNN_torch(nn.Module):
         self.hidden_layers = nn.ModuleList() 
         self.hidden_layers.append(SpikingDenseTorch(self.N_in, self.N(1), (X_n[0] if type(X_n)==list else X_n)))
 
-        for i in range(self.N_layers-2):
+        for i in range(self.N_layers-2):        # If N_layers > 2, append the rest of the layers
             self.hidden_layers.append(SpikingDenseTorch(self.N(i+2), self.N(i+2), (X_n[i+1] if type(X_n)==list else X_n))) 
 
 
-        self.output_layer = SpikingDenseTorch(self.N(self.N_layers), self.N_out, robustness_params=robustness_params)
+        self.output_layer = SpikingDenseTorch(self.N(self.N_layers), self.N_out, robustness_params=robustness_params, is_output=True)
 
+    def forward(self, x):
+        ''' Defines the forward pass through the entire SNN architecture '''
+        for l in self.hidden_layers:
+            x = l(x)
+        
+        x = self.output_layer(x)
+        return x 
         
 
 def create_torch_fc_model_ReLU(layers=2, N_hid=340,N_in=784, N_out=10):
