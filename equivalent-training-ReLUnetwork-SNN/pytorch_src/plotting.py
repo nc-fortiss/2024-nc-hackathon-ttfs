babde7f94b41e1e0e28563668959f9a9eb073200
Aaron Schulze <aaronschulze01@gmail.com>
2025-04-05 21:40:43 +0200

    added function to plot membrane potential from saved path location with output spikes

--- equivalent-training-ReLUnetwork-SNN/pytorch_src/plotting.py
+++ equivalent-training-ReLUnetwork-SNN/pytorch_src/plotting.py
@@ -3,6 +3,7 @@ import numpy as np
 from model_torch import compute_membrane_potential
 import matplotlib.pyplot as plt 
 import matplotlib.cm as cm
+import matplotlib.gridspec as gridspec
 import config_utils
 import os 
 from model_torch import *
@@ -18,6 +19,7 @@ def plot_membrane_potential(model, target_neurons, title_addition=''):
         @model: FC_SNN_torch instance. The model should have at least 3 hidden layers (including the output_layer)
         @target_neurons: can be a list or an integer representing the target neuron
             for which the membrane potential(s) should be plotted
+        @title_addition: additional string for extending the plot title
     '''
     # Convert integer into list if a list was not passed (simplifies later code)
     if not isinstance(target_neurons, list):
@@ -132,43 +134,176 @@ def plot_membrane_potential(model, target_neurons, title_addition=''):
     plt.tight_layout() 
     fig.subplots_adjust(top=0.85) 
     plt.show()
+    
 
 
-
-def plot_input_spikes():
-    ''' Make a histogram plot to visualize the distribution of the input spike times layer-wise
-        Requires a 'spike_output.txt' logging file to have been generated (e.g. during a single forward pass)
+def plot_membrane_potential_path(model, data_path, target_neurons, title_addition=''):
     '''
+        Plots the membrane potential dynamics for 'target_neurons'.
 
-    spikes_file_path = os.path.join(config_utils.LOGGING_DIR, 'spike_output.txt')
+        Parameters: 
+        @model: FC_SNN_torch instance. The model should have at least 3 hidden layers (including the output_layer)
+        @data_path: .npz file containing the layer-wise activations / spike timestamps
+        @target_neurons: can be a list or an integer representing the target neuron
+            for which the membrane potential(s) should be plotted
+        @title_addition: additional string title for plot
+    '''
+    
+    # Convert integer into list if a list was not passed (simplifies later code)
+    if not isinstance(target_neurons, list):
+        target_neurons = [target_neurons]
 
-    if not os.path.isfile(spikes_file_path):
+    # read-in the data and convert into 
+    with open(data_path, "rb") as f:
+        activations = pickle.load(f)
+        
+
+    # Spike activations arriving from layer (N-1)
+    input_activations = activations['layer_0']
+    sorted_activations = np.sort(input_activations)
+    sorted_indices = np.argsort(input_activations)
+
+    # New output spikes generated in layer (N)
+    output_activations = activations['layer_1']
+    sorted_output_activations = np.sort(output_activations)
+
+    # Get the spike threshold for the i-th neuron. In particular, 'D_i' is the trainable parameter that is unique to each neuron
+    thresholds = {}
+    for neuron in target_neurons:
+        thresholds[neuron] = model.hidden_layers[1].t_max - model.hidden_layers[1].t_min - model.hidden_layers[1].D_i[neuron].detach().numpy()
+
+    # For all neurons (i) in 'target_neurons':
+    # Loop over a selection of neurons (j) from layer (N-1) which have sent a spike to neuron(i) in layer(N)
+    # through the synapse (W_ji); collect spike times and membrane potentials for these neurons
+
+    spike_times = {}
+    potentials = {}
+    for target_neuron_index in target_neurons:
+        
+        spike_times_i = []        # input spiking timestamps (tj) from layer (N-1) to neuron(i)
+        potentials_i = []         # change in membrane potential neuron (i) in layer (N)
+        for j in range(1,340,10):
+            V_i = compute_membrane_potential(
+                    sorted_activations[j],      # get next input spike time tj at which V should be evaluated
+                    target_neuron_index,        
+                    input_activations,         # all input spikes tj from layer (N-1)  
+                    model.hidden_layers[1].kernel.data.detach().numpy()     # kernel W connecting layer (N-1) to layer (N)
+                )
+
+            spike_times_i.append(sorted_activations[j])
+            potentials_i.append(V_i)
+
+        spike_times[target_neuron_index] = spike_times_i 
+        potentials[target_neuron_index] = potentials_i
+    
+    # print("Spike Times: ", spike_times)
+    # print("Potentials: ", potentials)
+    # breakpoint()
+
+    # Start the plot 
+    fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+
+    # --- Interval boundaries (t_min, t_max) --- #
+    ax.axvline(x=model.hidden_layers[0].t_min, linestyle="--", color="black")
+    ax.axvline(x=model.hidden_layers[0].t_max, linestyle="--", color="black")
+    ax.axvline(x=model.hidden_layers[1].t_max, linestyle="--", color="black")
+
+    
+    
+    # --- Threshold of layer (N) --- #
+    ax.axhline(y=thresholds[target_neurons[0]], label=f"threshold", color='green', lw=4, alpha=0.7)
+
+    # --- Membrane potential changes --- #
+    colors = cm.tab10(np.linspace(0, 1, len(target_neurons))) 
+    colors = dict(zip(target_neurons, colors))
+    for i in target_neurons:
+        ax.scatter(spike_times[i], potentials[i], alpha=0.5, color=colors[i])
+    
+    # --- Constant slope - fire phase --- #
+    for i in target_neurons:
+        x_spike = thresholds[i] + (model.hidden_layers[0].t_max - potentials[i][-1])
+        y_spike = x_spike - model.hidden_layers[0].t_max + potentials[i][-1]
+        ax.plot([model.hidden_layers[0].t_max, x_spike], 
+                [potentials[i][-1], y_spike], 
+                color=colors[i], alpha=0.5, lw=4,
+                label=f"V_{i}")
+    
+    y_min, y_max = ax.get_ylim()
+    output_spike_times = []
+    # --- Generated Spike Output Times in layer(N) --- #
+    for i in target_neurons:
+        # get the spike times that were produced by each neuron after integration and threshold crossing
+        output_spike_from_neuron = output_activations[i]
+        output_spike_times.append(output_spike_from_neuron)
+        ax.plot(output_spike_from_neuron, 0, color=colors[i], marker='^', markersize=15, label=f"Spike_{i}")
+        ax.plot([output_spike_from_neuron, output_spike_from_neuron],
+                [y_min, 0], linestyle="--", color=colors[i], alpha=0.5)
+
+
+    # --- Axis labels --- #
+    ax.set_xlabel('Spike Activation Time (t)', labelpad=20)
+    ax.set_ylabel('Membrane Potential V(t)', labelpad=20)
+
+    # Label the vertical lines for interval boundaries
+    y_pos = (y_min) + (y_max - y_min) / 1.1     # position of (t_min) or (t_max) label
+    ax.text(model.hidden_layers[0].t_min + 0.3, y_pos, 't_min_0', va='center', ha='left')
+    ax.text(model.hidden_layers[0].t_max + 0.3, y_pos, 't_max_0\nt_min_1', va='center', ha='left')
+    ax.text(model.hidden_layers[1].t_max + 0.3, y_pos, 't_max_1', va='center', ha='left')
+
+    # xticks for generated spike times
+    current_xticks = ax.get_xticks()
+    new_xticks = sorted(set(current_xticks.tolist() + output_spike_times))
+    ax.set_xticks(new_xticks)
+    ax.set_xlim(0.0)
+
+    
+    title = 'Membrane Potential vs Spike Activations\nfor selected Neurons during Inference on a single MNIST image'
+    if title_addition: title = title + "\n" + title_addition
+    fig.suptitle(title)
+
+    plt.legend(loc='center right', bbox_to_anchor=(1, 0.5))
+
+    plt.tight_layout() 
+    fig.subplots_adjust(top=0.85) 
+    plt.show()
+
+    
+
+def plot_output_spikes(data_path, log_scale=False, additional_title=''):
+    ''' Make a histogram plot to visualize the distribution of the input spike times layer-wise
+        Requires a .npz file at 'data_path' with the logged spike times in a dict format, where 
+        the keys correspond to a layer number such as 'layer_i' and the values in list format.
+    '''
+    if not os.path.isfile(data_path):
         config_utils.logging.info("### Could not find any logged files for plotting spike times - generate one first ###")
         return -1
 
-    spikes_per_layer = []
-
-    # convert input strings into a list of lists (list of spike times per layer)
-    # each line in the input corresponds to the spike values of a single layer
-    with open(spikes_file_path, 'r') as f:
-        for line in f:
-            line = line.strip("\n")  
-            spikes = [float(v) for v in line.split()]  
-            spikes_per_layer.append(spikes)
-
-
+    with open(data_path, "rb") as f:
+        activations = pickle.load(f)
+    
+    total = 0
     plt.figure(figsize=(10, 5))
-    # plt.hist(spikes_per_layer, bins=30, density=True)
+    for k,v in activations.items():
+        # breakpoint()
+        activations_np = np.round(np.array(v),decimals=2)
+        plt.hist(activations_np, bins=40, label=k, log=log_scale)
+        total += len(activations_np)
+    plt.title(f'Layer-wise Spike Time Distribution - N={total}' + additional_title)
 
-    plt.hist(spikes_per_layer[0], bins=80, density=True)
-    # plt.hist(spikes_per_layer[1], range=(2998, 3003), bins=30, density=True)
-
-    plt.title('Spike Time Activation Distribution for converted Input')
     plt.xlabel('Spike Time Activation')
     plt.ylabel('Frequency')
-
-    # plt.xlim(left = 0)
+    plt.legend()
     plt.show()
+
+
+def plot_input_tensor(t):
+    plt.imshow(t, cmap='gray_r', interpolation='nearest')  # 'hot' = heatmap style
+    plt.colorbar()
+    plt.title("Heatmap of Tensor")
+    plt.show()
+
+
+
 
 
 def main():
